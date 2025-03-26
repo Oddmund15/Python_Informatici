ee8ab545097a00c1356a6a3f8a297887be888458
Oddmund15 <michele.riccabone@edu.immaginazioneelavoro.it>
2025-03-26 13:51:36 +0100

    Update andLogico.py

--- python/Lezione 6/andLogico.py
+++ python/Lezione 6/andLogico.py
@@ -1,15 +1,15 @@
 # L' AND logico(&&) mi permette di unire più condizioni. Vuol dire che ho bisogno che entrambe
 #  le consizioni siamo valide affinchè tutta la condizione sia true 
 
-input("Come ti chiami")
-eta = int(input("Quanti anni hai?"))
-invito = bool(input("Hai ricevuto un invito ?"))
+nome = input("Come ti chiami: ")
+eta = int(input("Quanti anni hai?: "))
+invito = input("Hai ricevuto un invito? (sì/no): ").lower() == 'sì'
 
 if eta >= 18 and invito:
-    print("Ciao Stefan, puoi far parte del club dei programmatori")
+    print(f"Ciao {nome}, puoi far parte del club dei programmatori")
 elif eta < 18 and invito:
-    print("Caro Stefan, non hai ancora compiuto 18 anni. Anche se hai l'invito non puoi entrare")
+    print(f"Caro {nome}, non hai ancora compiuto 18 anni. Anche se hai l'invito non puoi entrare")
 elif eta >= 18 and not invito:
-    print("Caro Stefan, non hai ancora compiuto 18 anni e non hai l'invito per entrare")
+    print(f"Caro {nome}, non hai l'invito per entrare nel club")
 else:
-    print("Mi spiace, non puoi fare parte del club. Non hai 18 anni e non hai neppure un invito" )
+    print(f"Mi spiace {nome}, non puoi fare parte del club. Non hai 18 anni e non hai neppure un invito")
