980df1ffd714cf8e2256f3c4779cf459cd62eaab
Oddmund15 <michele.riccabone@edu.immaginazioneelavoro.it>
2025-03-26 13:43:37 +0100

    Update orLogico.py

--- python/Lezione 6/orLogico.py
+++ python/Lezione 6/orLogico.py
@@ -6,14 +6,20 @@ nome = input("Qual'è il tuo nome: ")
 esameScritto = input("Hai passato l'esame scritto (si/no): ") == "si"
 esameOrale = input("Hai passato l'esame orale (si/no): ") == "si"
 
+# Caso con il professore "Bravo"
 print("Professore Bravo")
 if esameScritto or esameOrale:
     print("Complimenti, hai superato l'esame")
 else:
     print("Mi spiace, non hai superato l'esame")
 
+# Caso con il professore "Cattivo"
 print("Professore Cattivo")
 if esameScritto and esameOrale:
     print("Complimenti, hai superato l'esame")
 else:
     print("Mi spiace, non hai superato l'esame perché uno dei due è andato male")
+    if not esameScritto:
+        print("Non hai passato l'esame scritto.")
+    if not esameOrale:
+        print("Non hai passato l'esame orale.")
